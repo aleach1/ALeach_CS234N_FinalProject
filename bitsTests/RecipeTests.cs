4c903091484bf41d6dc6a6405401dcf0421234ea
Aiden Leach <leacha@my.lanecc.edu>
2024-12-08 15:00:43 -0800

    Problems with connection string

---
+++ bitsTests/RecipeTests.cs
@@ -0,0 +1,50 @@
+using bitsEFClasses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bitsTests
+{
+    [TestFixture]
+    public class RecipeTests
+    {
+
+        BitsContext dbContext;
+        Recipe? r;
+        List<Recipe>? recipes;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            dbContext = new BitsContext();
+        }//Name from Recipe, ABV from recipe, IBU from
+
+        [Test]
+        public void GetAllTest()
+        {
+            recipes = dbContext.Recipes.OrderBy(r => r.Name).ToList();
+            Assert.AreEqual(4, recipes.Count);
+            Assert.AreEqual(1, recipes[0].RecipeId);
+
+        }
+
+        [Test]
+        public void GetRecipeById()
+        {
+            r = dbContext.Recipes.Find(1);
+            Assert.IsNotNull(r);
+            Assert.AreEqual(r.Name, "Fuzzy Tales Juicy IPA");
+
+        }
+
+        [Test]
+        public void GetWithInvoicesTest()
+        {
+            r = dbContext.Recipes.Include("RecipeIngredients").Where(re => re.RecipeId == 1).SingleOrDefault();
+            //c = dbContext.Customers.Include("Invoices").Where(c => c.CustomerId == 20).SingleOrDefault();
+            Assert.IsNotNull(r);
+            Assert.AreEqual("Fuzzy Tales Juicy IPA", r.Name);
+            Assert.AreEqual(13, r.RecipeIngredients.Count);
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ bitsTests/Test.cs
@@ -0,0 +1,28 @@
+﻿using bitsEFClasses;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitsTests
+{ 
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void ConnectionStringTest()
+        {
+            string folder = System.AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(folder)
+                    .AddJsonFile("mySqlSettings.json", optional: true, reloadOnChange: true);
+
+            string connectionString = builder.Build().GetConnectionString("mySql");
+            Console.WriteLine(connectionString);
+            Assert.NotNull(connectionString);
+        }
+    }
+}
